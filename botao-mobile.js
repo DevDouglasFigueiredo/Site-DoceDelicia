c4bc0a82fef4015fe89cbcf195cac89a9ca9427b
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2022-03-03 18:46:33 -0300

    falta deixar responsivo

---
+++ botao-mobile.js
@@ -0,0 +1,6 @@
+const botao__mobile = document.querySelector('.botao__mobile');
+const menu__navegacao = document.querySelector('.menu__navegacao');
+
+botao__mobile.addEventListener('click' , () => {
+    menu__navegacao.classList.toggle('active')
+})
